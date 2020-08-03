c92908fb6c766c12e6603df5a9a4a090bd28c6f9
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-08-04 01:45:26 +0300

    Begin transaction create component design

--- src/app/pages/transaction/transaction.module.ts
+++ src/app/pages/transaction/transaction.module.ts
@@ -1,21 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { NbMenuModule, NbButtonModule } from '@nebular/theme';
+import { NbMenuModule, NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbDatepickerModule, NbIconModule,
+  NbInputModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbUserModule } from '@nebular/theme';
 
 import { TransactionRoutingModule } from './transaction-routing.module';
 
 import { TransactionComponent } from './transaction/transaction.component';
+import { TransactionCreateComponent } from './transaction-create/transaction-create.component';
 
 @NgModule({
   declarations: [
     TransactionComponent,
+    TransactionCreateComponent
   ],
   imports: [
     TransactionRoutingModule,
     CommonModule,
+    NbMenuModule, 
+    NbActionsModule,
     NbButtonModule,
-    NbMenuModule
+    NbCardModule,
+    NbCheckboxModule,
+    NbDatepickerModule,
+    NbIconModule,
+    NbInputModule,
+    NbRadioModule,
+    NbSelectModule,
+    NbUserModule
   ]
 })
 export class TransactionModule {
