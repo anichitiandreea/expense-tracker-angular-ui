48ebb9147bc494449883b27c4e93687c94026c04
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-08-23 10:23:41 +0300

    Create dashboard component design

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,20 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { NbMenuModule } from '@nebular/theme';
+import { NbMenuModule, NbCardModule,
+  NbButtonModule, NbIconModule } from '@nebular/theme';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
-import { DashboardComponent } from './dashboard.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    ChartComponent,
   ],
   imports: [
     DashboardRoutingModule,
+    NbIconModule,
     CommonModule,
-    NbMenuModule
+    NbMenuModule,
+    NbCardModule, 
+  NbButtonModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts')})
   ]
 })
 export class DashboardModule {
