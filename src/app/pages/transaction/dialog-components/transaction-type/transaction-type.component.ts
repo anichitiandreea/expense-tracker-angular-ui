f4ceb94f3dcad5a3dafa0cf699e7a15a935923b2
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-08-26 13:22:36 +0300

    Add dialog components for transaction create

---
+++ src/app/pages/transaction/dialog-components/transaction-type/transaction-type.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { NbDialogRef } from '@nebular/theme';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-transaction-type',
+  templateUrl: './transaction-type.component.html',
+  styleUrls: ['./transaction-type.component.scss']
+})
+export class TransactionTypeComponent implements OnInit {
+	selectedTransaction: any;
+
+  constructor(protected dialogRef: NbDialogRef<TransactionTypeComponent>,
+    private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  close(): void {
+  	this.dialogRef.close();
+  }
+
+  selectTransaction(transactionType): void {
+    this.dialogRef.close();
+    this.router.navigateByUrl('/transactions/create', { state: { transactionType: transactionType } });
+  }
+}
