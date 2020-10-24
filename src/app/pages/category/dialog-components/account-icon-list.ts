349c90bc4e20ec645143fd19fbc12b91c6e13d15
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-10-24 17:43:37 +0300

    Create icon list for account

---
+++ src/app/pages/category/dialog-components/account-icon-list.ts
@@ -0,0 +1,11 @@
+export const accountIconList = [
+	{class: "fab fa-cc-mastercard", active: ""},
+	{class: "fab fa-cc-visa", active: ""},
+	{class: "fab fa-cc-amazon-pay", active: ""},
+	{class: "fas fa-euro-sign", active: ""},
+	{class: "fab fa-cc-paypal", active: ""},
+	{class: "fab fa-cc-jcb", active: ""},
+	{class: "fas fa-money-bill-alt", active: ""},
+	{class: "fas fa-coins", active: ""},
+	{class: "far fa-credit-card", active: ""},
+]
