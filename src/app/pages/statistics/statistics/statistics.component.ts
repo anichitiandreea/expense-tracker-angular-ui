55aa226bda197a660cfd15f76e3b526343ac2b3f
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-12-27 00:56:40 +0200

    Display daily expense on statistics page

--- src/app/pages/statistics/statistics/statistics.component.ts
+++ src/app/pages/statistics/statistics/statistics.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { mergeMap } from 'rxjs/operators';
+
 import { StatisticsService } from 'src/app/services/statistics.service';
+import { CurrencyService } from 'src/app/services/currency.service';
 
 @Component({
   selector: 'app-statistics',
@@ -9,21 +12,43 @@ import { StatisticsService } from 'src/app/services/statistics.service';
 })
 export class StatisticsComponent implements OnInit {
 	categories: any;
+  exchanges: any;
+  dailyExpense: string;
 
-  constructor(private statisticsService: StatisticsService) { }
+  constructor(
+    private statisticsService: StatisticsService,
+    private currencyService: CurrencyService) { }
 
   ngOnInit(): void {
   	let date = new Date(), currentMonth = date.getMonth(), currentYear = date.getFullYear();
     let fromDate = new Date(currentYear, currentMonth, 1);
     let toDate = new Date(currentYear, currentMonth + 1, 1);
 
-    console.log(fromDate)
-    console.log(toDate)
+    this.currencyService
+      .exchangeToRON()
+      .pipe(
+        mergeMap(response => {
+          this.exchanges = response;
 
-  	this.statisticsService.get(fromDate.toISOString(), toDate.toISOString())
-  		.subscribe(response => {
-  			this.categories = response;
-  		})
+          return this.statisticsService.get(fromDate.toISOString(), toDate.toISOString());
+        })
+      )
+      .subscribe(response => {
+        this.categories = response;
+        this.exchangeToLei();
+      });
   }
 
+  exchangeToLei(): void {
+    var totalMonthExpense = 0;
+    this.categories.forEach(category => {
+      if (category.categoryCurrency != "RON") {
+        let currencyRate = this.exchanges.rates[category.categoryCurrency];
+        totalMonthExpense += category.categoryAmount / currencyRate;
+      }
+    });
+
+    this.dailyExpense = (totalMonthExpense / 30).toFixed(2);
+    console.log(this.dailyExpense);
+  }
 }
